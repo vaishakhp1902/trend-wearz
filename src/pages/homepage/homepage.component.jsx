df5b949d592b5515f8605d3c318856227b0e8977
Vaishakh <vaishakhp1998@gmail.com>
2021-11-05 10:49:36 +0530

    updating with shop page, collection preview component and collection item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,7 +5,7 @@ import Directory from '../../components/directory/directory.component'
 // link which user clisk( hats/women) etc.. is passed to homepage component, so it is being passed to its parent components
 const HomePage = () => (
     <div className="homepage">
-        <Directory/>
+        <Directory />
     </div>
 )
 
